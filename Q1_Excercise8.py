985cdde67d182aa33f668c828c5800b30214894a
Melissa Stephens <mpullins@nd.edu>
2017-10-13 11:43:58 -0400

    added some pseudocode to the script for sanity

--- Q1_Excercise8.py
+++ Q1_Excercise8.py
@@ -9,9 +9,13 @@ Outfile=open("CfloridaCounts.txt","w")
 
 #for loop
 for line in Infile:
-	line.strip()
-	if ## in line:
+	line=line.strip()
+	if "##" in line:
 		Outfile.write(line + "\n")
-	elif # in line:
-		
-		
+	elif "#" in line:
+	#need to search through the line for all possible combinations of names (regex string)
+	#need to backreference and sub each position in the string
+		re.search(regex string, search string)
+		re.sub(regex string, replacement, search string)
+		Outfile.write(line + "\n")
+	else:
