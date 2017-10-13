13170adfcb5ae63f60aa62155626c4b293aed013
Melissa Stephens <mpullins@nd.edu>
2017-10-13 11:21:29 -0400

    started for loop for Q1

--- Q1_Excercise8.py
+++ Q1_Excercise8.py
@@ -1,8 +1,17 @@
 #Exercise 8
 #Authors: Janice Love and Melissa Stephens
 
+import re
+
 #Open files to read and write
 Infile=open("Cflorida.vcf","r")
 Outfile=open("CfloridaCounts.txt","w")
 
 #for loop
+for line in Infile:
+	line.strip()
+	if ## in line:
+		Outfile.write(line + "\n")
+	elif # in line:
+		
+		
