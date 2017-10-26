31ebface8b3873676fc90ee83cf6d5c71b249f1d
Brittni Bertolet <bbertole@nd.edu>
2017-10-26 10:24:27 -0400

    Fixed for loop

--- exercise8.py
+++ exercise8.py
@@ -1,6 +1,6 @@
 #### Tutorial 8
 # Set wd for Brittni: 
-#os.chdir('/Users/brittnibertolet/Desktop/bcTutorials/Intro_Biocomp_ND_318_Tutorial8/')
+os.chdir('/Users/brittnibertolet/Desktop/bcTutorials/Intro_Biocomp_ND_318_Tutorial8/')
 
 
 import re
@@ -17,13 +17,19 @@ r2=r"[Cc][Ff][0-9]?/.[Gg].?/.([0-9]{3})"
 for Line in vcffile:
     Line = Line.strip() #strip end of line
     if '##' in Line: # how can you tell if this is the header line?
-        print(Line) # write unchanged header line to file
-    elif '#' in Line: #how can you tell if this is the line with the column headings?
+        outfile.write(Line + "\n") # write unchanged header line to file
+    elif '#CHROM' in Line: #how can you tell if this is the line with the column headings?
         #standardize (replace) sample names with TX and FL regexes
-        Line=re.sub(r1, "Cf.Sfa./1", Line)
-        Line=re.sub(r2, "Cf.Gai./1", Line)
+        outfile.write(Line + "\n") 
+    else:
+        print("finished!")
+        outfile.close()
+###############
+
+        Line2=re.sub(r1, "Cf.Sfa./1", Line)
+        Line3=re.sub(r2, "Cf.Gai./1", Line2)
         #write new version of line to file
-        print(Line)
+        print(Line3)
     else: #now you're in the data
         #replace full SNP info with allele counts only
         #replace missing data with NA
