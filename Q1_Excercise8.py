5b7e3af30c45a1aee561b3593235c5a0dcd784a4
janicelove <jlove5@nd.edu>
2017-10-26 21:22:24 -0400

    Finished Q1, output file looks right

--- Q1_Excercise8.py
+++ Q1_Excercise8.py
@@ -8,11 +8,9 @@ Infile=open("Cflorida.vcf","r")
 Outfile=open("CfloridaCounts.txt","w")
 
 #set variables
-TxString=[Cc][Ff][0-9]*\.[Aa][0-9]*\.
-NewTxString=(Cf)\.(Sfa)\.
-
-FlString=[Cc][Ff]\.[Gg][Aa]*[A-Za-z]*[0-9]*\.)
-NewFlString=(Cf)\.(Gai)\.
+TxString=r"[Cc][Ff][0-9]*\.[Aa][0-9]*\."
+FlString=r"[Cc][Ff]\.[Gg][Aa]*[A-Za-z]*[0-9]*\."
+part2 = r"[-1.]/[01.]:([0-9},.]+):[0-9.]+:[0-9.]+:[0-9,.]+"
 
 #for loop
 for line in Infile:
@@ -22,6 +20,19 @@ for line in Infile:
         elif "#" in line:
         #need to search through the line for all possible combinations of names (regex string)
 	#then replace only the first two sets of information (not the XXX which is unique to each sample)
-                re.search([Cc][Ff][0-9]*\.[Aa][0-9]*\., line)
-                re.sub([Cc][Ff][0-9]*\.[Aa][0-9]*\.,(Cf)\.(Sfa)\.,line)
-                Outfile.write(line + "\n")
+                newTXstring = re.sub(TxString, "Cf.Sfa", line)
+                newNamesFinal = re.sub(FlString, "Cf.Gai", newTXstring)
+                Outfile.write(newNamesFinal + "\n")
+    
+        else: #the rest of the data: SNP, allele counts
+            part2_SNP = re.sub(part2, r"\1", line)
+        #missing data with NA 
+            part2NA = re.sub(r"\.","NA",part2_SNP)
+        #write to new file 
+            Outfile.write(part2NA + '\n')
+                
+
+
+
+Infile.close()
+Outfile.close()
