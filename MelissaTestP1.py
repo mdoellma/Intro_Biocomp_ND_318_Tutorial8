fc69b0a0da4bc3a1258260e5d66f8bed55c8d670
Melissa Stephens <mpullins@nd.edu>
2017-10-26 21:47:37 -0400

    fixed some errors from the regex and search strings. Still can't get re.sub to work

--- MelissaTestP1.py
+++ MelissaTestP1.py
@@ -5,11 +5,11 @@ Infile=open("Cflorida.vcf","r")
 Outfile=open("CfloridaCounts.txt","w")
 
 #set variables
-TxString=[Cc][Ff][0-9]*\.[Aa][0-9]*\.
-NewTxString=(Cf)\.(Sfa)\.
+TxString="[Cc][Ff][0-9]*\.[Aa][0-9]*\."
+NewTxString="(Cf)\.(Sfa)\."
 
-FlString=[Cc][Ff]\.[Gg][Aa]*[A-Za-z]*[0-9]*\.)
-NewFlString=(Cf)\.(Gai)\.
+FlString="[Cc][Ff]\.[Gg][Aa]*[A-Za-z]*[0-9]*\."
+NewFlString="(Cf)\.(Gai)\."
 
 #for loop
 for line in Infile:
@@ -18,6 +18,6 @@ for line in Infile:
                 Outfile.write(line + "\n")
         elif "#" in line:
         #need to search through the line for all possible combinations of names (regex string)
-                re.search([Cc][Ff][0-9]*\.[Aa][0-9]*\., line)
-                re.sub([Cc][Ff][0-9]*\.[Aa][0-9]*\.,(Cf)\.(Sfa)\.,line)
+                re.search("[Cc][Ff][0-9]*\.[Aa][0-9]*\.", line)
+                re.sub("[Cc][Ff][0-9]*\.[Aa][0-9]*\.","(Cf)\.(Sfa)\.",line)
                 Outfile.write(line + "\n")
